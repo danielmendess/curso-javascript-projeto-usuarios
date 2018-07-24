0dbf67253141a27546b5c804465e82df7de88e96
Daniel Mendes <dmsdaniel11@gmail.com>
2018-07-24 20:00:48 -0300

    substituindo funcao de callback por Promise

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -17,41 +17,54 @@ class UserController {
 
             let values = this.getValues();
 
-            
-            this.getPhoto((content) => {
+            this.getPhoto().then((content) => {
 
                 values.photo = content;
 
                 this.addLine(values);
 
-            });
+            }, (e) => {
 
+                console.error(e);
+
+            });
         
        });    
         
     }
 
-    getPhoto(callback) {
+    getPhoto() {
 
-        let fileReader = new FileReader();
+        return new Promise((resolve, reject) => {
 
-        let elements = [...this.formEl.elements].filter(item => {
+            let fileReader = new FileReader();
+    
+            let elements = [...this.formEl.elements].filter(item => {
+    
+                if (item.name === 'photo') {
+                    return item;
+                }
+    
+            });
+    
+            let file = elements[0].files[0];
+    
+            fileReader.onload = () => {
+    
+                resolve(fileReader.result);
+    
+            };
 
-            if (item.name === 'photo') {
-                return item;
+            fileReader.onerror = (e) => {
+
+                reject(e);
+
             }
+    
+            fileReader.readAsDataURL(file);
 
         });
 
-        let file = elements[0].files[0];
-
-        fileReader.onload = () => {
-
-            callback(fileReader.result);
-
-        }
-
-        fileReader.readAsDataURL(file);
 
     }
 
