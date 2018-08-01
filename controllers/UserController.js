2bfa2ebdbe6bfebb7173d4d422c1df2c9c47ea74
Daniel Mendes <dmsdaniel11@gmail.com>
2018-07-31 23:43:10 -0300

    Atualizando estatisticas e programando botão editar

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -6,6 +6,17 @@ class UserController {
         this.tableEl = document.getElementById(tableId);
 
         this.onSubmit();
+        this.onEdit();
+
+    }
+
+    onEdit() {
+
+        document.querySelector("#box-user-update .btn-cancel").addEventListener('click', e => {
+
+            this.showPanelCreate();
+
+        });
 
     }
 
@@ -143,6 +154,8 @@ class UserController {
 
         let tr = document.createElement('tr');
 
+        tr.dataset.user = JSON.stringify(dataUser);
+
         tr.innerHTML = `
                 <td><img src="${dataUser.photo}" alt="Imagem do usuário" class="img-circle img-sm"></td>
                 <td>${dataUser.name}</td>
@@ -150,13 +163,95 @@ class UserController {
                 <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
                 <td>${Utils.dateFormat(dataUser.register)}</td>
                 <td>
-                    <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                    <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
                     <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
                 </td>
         `;
 
+        tr.querySelector('.btn-edit').addEventListener('click', e => {
+
+            let json = JSON.parse(tr.dataset.user);
+            let form = document.querySelector('#form-user-update');
+
+            for(let name in json) {
+                
+                let field = form.querySelector("[name=" + name.replace("_", "") + "]");
+
+                
+                if (field) {
+                    
+                    switch (field.type) {
+                        case 'file':
+                            continue;
+                            break;
+                        
+                        case 'radio':
+                            form.querySelector("[name=" + name.replace("_", "") + "][value=" + json[name] + "]");
+                            field.checked = true;
+                            break;
+
+                        case 'checkbox':
+                            field.checked = json[name];
+                            break;
+                    
+                        default:
+                            field.value = json[name];
+                            break;
+                    }
+
+                    
+
+                }
+
+                
+            }
+
+            this.showPanelUpdate();
+            
+
+        });
+
         this.tableEl.appendChild(tr);
+
+        this.updateCount();
 
     }
 
+    showPanelCreate() {
+
+        document.querySelector('#box-user-create').style.display = "block";
+        document.querySelector('#box-user-update').style.display = "none";
+
+    }
+
+    showPanelUpdate() {
+
+        document.querySelector('#box-user-create').style.display = "none";
+        document.querySelector('#box-user-update').style.display = "block";
+
+    }
+
+    updateCount() {
+
+        let numberUsers = 0;
+        let numberAdmin = 0;
+
+        [...this.tableEl.children].forEach(tr => {
+
+            numberUsers++;
+
+            let user = JSON.parse(tr.dataset.user);
+
+            if(user._admin) {
+
+                numberAdmin++;
+
+            }
+
+        });
+
+        document.querySelector('#number-users').innerHTML = numberUsers;
+        document.querySelector('#number-users-admin').innerHTML = numberAdmin;
+
+    }
 } 
